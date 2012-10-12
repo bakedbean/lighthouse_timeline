da4306e2edba75644d41943be1ed70a434c8b8c4
Eben Goodman <egoodman2@berklee.edu>
2012-10-12 14:53:09 -0400

    added ajax error handler on google api request

--- public/js/custom.js
+++ public/js/custom.js
@@ -7,6 +7,9 @@
 function tracker(number){
   $.ajax({
     url: '/tracker/'+number,
+    error: function(jqXHR,textStatus,errorThrown){
+      alert("The following error occurred: "+errorThrown+", Message: "+textStatus);
+    },
     success: function(data){
       console.log(data);
     }
